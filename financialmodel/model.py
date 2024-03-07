21526ce10ee8f0bc6a3dc96fd16d630a58181df8
fredric12 <91897881+fredric12@users.noreply.github.com>
2024-03-07 15:02:23 +0100

    init financial model

--- financialmodel/model.py
+++ financialmodel/model.py
@@ -1,14 +1,8 @@
-import os
-import sys
-import pandas as pd
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+def financialmodel():
 
-import powercalculations.powercalculations as pc
 
-file_path_irradiance = 'data/Irradiance_data_v2.xlsx'
-file_path_load = 'data/Load_profile_6_v2.xlsx'
-irradiance=pc.PowerCalculations(file_path_irradiance=file_path_irradiance,file_path_load=file_path_load)
-irradiance.fill_load_with_weighted_values()
-# Define custom formatting for printing all elements
-formatter = pd.option_context('display.max_rows', None, 'display.max_columns', None)
+    cost_grid=GridCost()
+    
+    NPV=
+    return NPV
