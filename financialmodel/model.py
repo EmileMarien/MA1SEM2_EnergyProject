ef17c20b9474a101fe7cfc648bea45fa4096f7a0
Fredric Demeyere <fredric.demeyere@hotmail.com>
2024-03-07 16:42:12 +0100

    edit PV_power

--- financialmodel/model.py
+++ financialmodel/model.py
@@ -3,7 +3,13 @@ def financialmodel(self):
 
 
     cost_grid=gridcost()
+    batterycost=500
+
+    NPV=
     
+    return NPV
+
+
 
 
 
--- powercalculations/_pvpower.py
+++ powercalculations/_pvpower.py
@@ -1,4 +1,4 @@
-def PV_generated_power(self,cell_area:int=1, panel_count:int=1, T_STC:int=25, V_OC_STC:int=0.6, delta_V_OC:int=-0.025, I_sc_a:int=300, FF:int=0.8, T_cell:int=30, irradiance_STC:int=100, irradiance_a:int=120):
+def PV_generated_power(self,cell_area:int=1, panel_count:int=1, T_STC:int=25, Temp_coeff:int = -0.026,efficiency_max:int = 0.2):
         """
         Converts the irradiance data to power data using the specified column name
         https://www.researchgate.net/post/How_can_I_calculate_the_power_output_of_a_PV_system_in_one_day_using_a_function_of_the_temperature_of_the_cell_and_the_reference_temperature
@@ -10,11 +10,11 @@ def PV_generated_power(self,cell_area:int=1, panel_count:int=1, T_STC:int=25, V_
         DataFrame: The DataFrame containing the converted power data
         """
         
-
+        T_cell=self.pd['T_RV_degC']
         # Check if the 'beamirradiance' column is empty
         if 'DirectIrradiance' in self.pd.columns and not self.pd['DirectIrradiance'].empty:
             # Calculate the PV generated power
-            self.pd['PV_generated_power'] = FF*cell_area*I_sc_a*self.pd['DirectIrradiance']/irradiance_STC*(V_OC_STC+delta_V_OC*(T_cell-T_STC))*panel_count
+            self.pd['PV_generated_power'] = efficiency_max*cell_area*self.pd['DirectIrradiance']*(1+Temp_coeff)*(T_cell-T_STC)*panel_count
         else:
             raise ValueError("The 'directIrradiance' column is empty or not present in the DataFrame")
         return None
