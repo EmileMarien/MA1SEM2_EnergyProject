7ffd370315fd8dd541bc8b96d3d3aec81a9565a2
EmileMarien <100191898+EmileMarien@users.noreply.github.com>
2024-03-25 22:56:40 +0100

    grid_cost changes

--- financialmodel/gridcost.py
+++ financialmodel/gridcost.py
@@ -21,17 +21,24 @@ def grid_cost(solar_count: int=1, panel_surface:int= 1 ,annual_degredation: int=
     #battery_capacity: capaciteit van de batterij 
     #battery_count: hoeveel batterijen aanwezig, enkel relevant voor stacked batteries
 
+    # GENK data
+    latitude=50.99461 # [degrees]
+    longitude=5.53972 # [degrees]
     file = open('data/combined_dataframe','rb')
     irradiance=pickle.load(file)
     file.close()
-    irradiance.fill_load_with_weighted_values()
+    print("a")
+    irradiance.filter_data_by_date_interval('2018-03-12 00:00', '2018-03-16 18:00', '1h')
     irradiance.calculate_direct_irradiance(tilt_angle=tilt_angle,latitude=latitude,longitude=longitude)
-    irradiance.PV_generated_power(N=solar_count, cell_area=panel_surface, efficiency_max=panel_efficiency*(1-annual_degredation),Temp_coeff=temperature_Coefficient)
+    irradiance.PV_generated_power(panel_count=solar_count, cell_area=panel_surface, efficiency_max=panel_efficiency*(1-annual_degredation),Temp_coeff=temperature_Coefficient)
     irradiance.power_flow(max_charge=battery_capacity*battery_count)
-    
+    print("b")
     financials=fa.FinancialAnalysis(irradiance.get_grid_power())
     financials.dual_tariff()
+    print("c")
     financials.dynamic_tariff()
     cost=financials.get_grid_cost_total()
 
     return cost
+
+grid_cost()
