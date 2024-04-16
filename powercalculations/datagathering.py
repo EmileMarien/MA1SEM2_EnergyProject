cd46d6465e149a473546879cf09a14cd847e51e9
jakob1113 <jakob.dhooghe@student.kuleuven.be>
2024-04-16 16:48:34 +0200

    hjk

--- powercalculations/datagathering.py
+++ powercalculations/datagathering.py
@@ -6,7 +6,7 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 
 import powercalculations.powercalculations as pc
 
-
+print('test')
 file_path_combined = 'data/combined_data_v9.xlsx'
 #irradiancefilepath='data/Irradiance_data_vtest.xlsx'
 #loadfilepath='data/Load_profile_6_vtest.xlsx'
@@ -24,17 +24,6 @@ longitude=5.53972 # [degrees]
 #file.close()
 #irradiance.plot_columns(["DirectIrradiance","GlobRad","DiffRad"])
 print('start')
-irradiance.calculate_direct_irradiance(latitude=latitude, tilt_angle=30,longitude=longitude,temperature=10,orientation='S')
-file=open('data/initialized_dataframes/pd_S_30','wb')
-pickle.dump(irradiance,file)
-file.close()
-print('upload 1 finished')
-
-irradiance.calculate_direct_irradiance(latitude=latitude, tilt_angle=30,longitude=longitude,temperature=10,orientation='EW')
-file=open('data/initialized_dataframes/pd_EW_30','wb')
-pickle.dump(irradiance,file)
-file.close()
-print('upload 2 finished')
 
 irradiance.calculate_direct_irradiance(latitude=latitude, tilt_angle=32,longitude=longitude,temperature=10,orientation='EW')
 file=open('data/initialized_dataframes/pd_EW_opt_32','wb')
